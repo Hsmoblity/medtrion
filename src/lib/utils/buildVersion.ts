1fc4a09c0109a190eddd101ec8e7e00085275ca8
shiv2050 <singhstrut@gmail.com>
2026-07-28 01:19:15 +0530

    fixed buid error

--- src/lib/utils/buildVersion.ts
+++ src/lib/utils/buildVersion.ts
@@ -1,7 +1,7 @@
 // Auto-generated file - DO NOT EDIT MANUALLY
-// Generated at build time from last git commit: 1785078675
-// Generated on: 2026-07-27T19:30:27.003Z
+// Generated at build time from last git commit: 1785181158
+// Generated on: 2026-07-27T19:47:39.494Z
 
-export const BUILD_VERSION = '2607262041';
-export const BUILD_TIMESTAMP = 1785078675;
-export const BUILD_DATE = '2026-07-26T15:11:15.000Z';
+export const BUILD_VERSION = '2807260109';
+export const BUILD_TIMESTAMP = 1785181158;
+export const BUILD_DATE = '2026-07-27T19:39:18.000Z';

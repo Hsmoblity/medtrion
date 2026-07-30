a243e91048a78ca32426f1fea98c8c7a37ed10bd
shiv2050 <singhstrut@gmail.com>
2026-07-31 03:03:55 +0530

    update hero slider

--- src/lib/utils/buildVersion.ts
+++ src/lib/utils/buildVersion.ts
@@ -1,7 +1,7 @@
 // Auto-generated file - DO NOT EDIT MANUALLY
-// Generated at build time from last git commit: 1785440562
-// Generated on: 2026-07-30T19:46:43.401Z
+// Generated at build time from last git commit: 1785440957
+// Generated on: 2026-07-30T21:22:52.417Z
 
-export const BUILD_VERSION = '3107260112';
-export const BUILD_TIMESTAMP = 1785440562;
-export const BUILD_DATE = '2026-07-30T19:42:42.000Z';
+export const BUILD_VERSION = '3107260119';
+export const BUILD_TIMESTAMP = 1785440957;
+export const BUILD_DATE = '2026-07-30T19:49:17.000Z';
